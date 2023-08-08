719fd3846681de9eb12498d8fd4a0f73ac230451
Neoly Moreno <neolyamr@gmail.com>
2023-08-08 14:57:43 -0500

    Initial commit

---
+++ src/test/java/com/yeralitas/yeralitaspringboot/YeralitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeralitas.yeralitaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeralitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
